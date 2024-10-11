f125648f11586ad4d116813c5b383b1fa7849292
omerkisa1 <omereis.5555@gmail.com>
2024-10-11 22:15:10 +0300

    hangman button activation and updates

--- Hangman/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ Hangman/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -31,8 +31,9 @@
             this.txtGuess = new System.Windows.Forms.TextBox();
             this.lblGuessPointer = new System.Windows.Forms.Label();
             this.btnCheckGuess = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.WordLabel = new System.Windows.Forms.Label();
+            this.wrongWordLabel = new System.Windows.Forms.Label();
+            this.hangmanLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtGuess
@@ -41,6 +42,7 @@
             this.txtGuess.Name = "txtGuess";
             this.txtGuess.Size = new System.Drawing.Size(100, 22);
             this.txtGuess.TabIndex = 0;
+            this.txtGuess.TextChanged += new System.EventHandler(this.txtGuess_TextChanged);
             // 
             // lblGuessPointer
             // 
@@ -57,29 +59,51 @@
             this.btnCheckGuess.Name = "btnCheckGuess";
             this.btnCheckGuess.Size = new System.Drawing.Size(146, 23);
             this.btnCheckGuess.TabIndex = 2;
-            this.btnCheckGuess.Text = "Check Your Guess";
+            this.btnCheckGuess.Text = "Hangman";
             this.btnCheckGuess.UseVisualStyleBackColor = true;
+            this.btnCheckGuess.Click += new System.EventHandler(this.btnCheckGuess_Click);
             // 
-            // pictureBox1
+            // WordLabel
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(116, 278);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
+            this.WordLabel.Location = new System.Drawing.Point(476, 207);
+            this.WordLabel.Name = "WordLabel";
+            this.WordLabel.Size = new System.Drawing.Size(168, 25);
+            this.WordLabel.TabIndex = 4;
+            this.WordLabel.Text = "___________";
+            this.WordLabel.Click += new System.EventHandler(this.WordLabel_Click);
+            // 
+            // wrongWordLabel
+            // 
+            this.wrongWordLabel.AutoSize = true;
+            this.wrongWordLabel.Location = new System.Drawing.Point(116, 371);
+            this.wrongWordLabel.Name = "wrongWordLabel";
+            this.wrongWordLabel.Size = new System.Drawing.Size(0, 17);
+            this.wrongWordLabel.TabIndex = 5;
+            this.wrongWordLabel.Click += new System.EventHandler(this.wrongWordLabel_Click);
+            // 
+            // hangmanLabel
+            // 
+            this.hangmanLabel.AutoSize = true;
+            this.hangmanLabel.Location = new System.Drawing.Point(129, 229);
+            this.hangmanLabel.Name = "hangmanLabel";
+            this.hangmanLabel.Size = new System.Drawing.Size(0, 17);
+            this.hangmanLabel.TabIndex = 6;
+            this.hangmanLabel.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.hangmanLabel);
+            this.Controls.Add(this.wrongWordLabel);
+            this.Controls.Add(this.WordLabel);
             this.Controls.Add(this.btnCheckGuess);
             this.Controls.Add(this.lblGuessPointer);
             this.Controls.Add(this.txtGuess);
             this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Text = "Hangman";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,7 +114,9 @@
         private System.Windows.Forms.TextBox txtGuess;
         private System.Windows.Forms.Label lblGuessPointer;
         private System.Windows.Forms.Button btnCheckGuess;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label WordLabel;
+        private System.Windows.Forms.Label wrongWordLabel;
+        private System.Windows.Forms.Label hangmanLabel;
     }
 }
 
--- Hangman/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ Hangman/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
@@ -15,6 +8,170 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private string selectedWord;
+        private char[] displayWord;
+        private string displayMessage = "";
+        private int wrongGuessCount = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] words = { "PROGRAMMING", "SOFTWARE", "NESNE", "YAPAY ZEKA" };
+            Random random = new Random();
+            selectedWord = words[random.Next(words.Length)];
+
+            displayWord = new string('_', selectedWord.Length).ToCharArray();
+            WordLabel.Text = string.Join(" ", displayWord);
+        }
+
+        private void txtGuess_TextChanged(object sender, EventArgs e)
+        {
+            string guess = txtGuess.Text;
+
+            if (guess.Length > 1)
+            {
+                txtGuess.Text = guess.Substring(0, 1);
+                txtGuess.SelectionStart = txtGuess.Text.Length;
+            }
+            if (guess.Length > 0 && !char.IsLetter(guess[0]))
+            {
+                txtGuess.Clear();
+            }
+        }
+
+        private void btnCheckGuess_Click(object sender, EventArgs e)
+        {
+            string guess = txtGuess.Text.ToUpper();
+
+            if (string.IsNullOrWhiteSpace(guess))
+            {
+                MessageBox.Show("Empty guess made!");
+                return;
+            }
+
+            if (selectedWord.Contains(guess))
+            {
+                for (int i = 0; i < selectedWord.Length; i++)
+                {
+                    if (selectedWord[i].ToString() == guess)
+                    {
+                        displayWord[i] = guess[0];
+                    }
+                }
+
+                WordLabel.Text = string.Join(" ", displayWord);
+            }
+            else
+            {
+                if (!displayMessage.Contains(guess))
+                {
+                    displayMessage += guess + " ";
+                    wrongWordLabel.Text = displayMessage;
+                    wrongGuessCount++;
+
+                    DisplayHangman(wrongGuessCount);
+
+                    if (wrongGuessCount >= 7)
+                    {
+                        MessageBox.Show("Game Over! The correct word was: " + selectedWord);
+                        btnCheckGuess.Enabled = false;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("This letter is already in the list: " + guess);
+                }
+            }
+
+            txtGuess.Clear();
+        }
+
+        // Hangman'i gösteren fonksiyon
+        private void DisplayHangman(int tries)
+        {
+            string[] stages =
+            {
+                @"
+               --------
+               |      |
+               |      
+               |    
+               |      
+               |     
+               -",
+                @"
+               --------
+               |      |
+               |      O
+               |    
+               |      
+               |     
+               -",
+                @"
+               --------
+               |      |
+               |      O
+               |      |
+               |      
+               |     
+               -",
+                @"
+               --------
+               |      |
+               |      O
+               |     \|
+               |      
+               |     
+               -",
+                @"
+               --------
+               |      |
+               |      O
+               |     \|/
+               |      
+               |     
+               -",
+                @"
+               --------
+               |      |
+               |      O
+               |     \|/
+               |      |
+               |     
+               -",
+                @"
+               --------
+               |      |
+               |      O
+               |     \|/
+               |      |
+               |     / 
+               -",
+                @"
+               --------
+               |      |
+               |      O
+               |     \|/
+               |      |
+               |     / \
+               -"
+            };
+
+            hangmanLabel.Text = stages[tries]; // ASCII sanatını gösteren label
+        }
+
+        private void wrongWordLabel_Click(object sender, EventArgs e)
+        {
+            // Button actions
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+        private void WordLabel_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
