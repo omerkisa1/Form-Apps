f441e81b2b2220ec54dea71a1b8d8411d1618f40
omerkisa1 <omereis.5555@gmail.com>
2024-10-11 20:23:56 +0300

    first design

--- Hangman/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ Hangman/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.lblGuessPointer = new System.Windows.Forms.Label();
+            this.btnCheckGuess = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(479, 261);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(100, 22);
+            this.txtGuess.TabIndex = 0;
+            // 
+            // lblGuessPointer
+            // 
+            this.lblGuessPointer.AutoSize = true;
+            this.lblGuessPointer.Location = new System.Drawing.Point(420, 266);
+            this.lblGuessPointer.Name = "lblGuessPointer";
+            this.lblGuessPointer.Size = new System.Drawing.Size(53, 17);
+            this.lblGuessPointer.TabIndex = 1;
+            this.lblGuessPointer.Text = "Guess:";
+            // 
+            // btnCheckGuess
+            // 
+            this.btnCheckGuess.Location = new System.Drawing.Point(479, 305);
+            this.btnCheckGuess.Name = "btnCheckGuess";
+            this.btnCheckGuess.Size = new System.Drawing.Size(146, 23);
+            this.btnCheckGuess.TabIndex = 2;
+            this.btnCheckGuess.Text = "Check Your Guess";
+            this.btnCheckGuess.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(116, 278);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnCheckGuess);
+            this.Controls.Add(this.lblGuessPointer);
+            this.Controls.Add(this.txtGuess);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Label lblGuessPointer;
+        private System.Windows.Forms.Button btnCheckGuess;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
