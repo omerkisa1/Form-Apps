201791731288835e407b1091bbd3578bcac9d271
omerkisa1 <omereis.5555@gmail.com>
2024-10-23 14:21:23 +0300

    week 2 question 3 added

---
+++ Week-2/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check if a number is an Armstrong number or not: ");
+
+            int givenNumber = int.Parse(Console.ReadLine());
+
+     
+            int digitCount = Math.Abs(givenNumber).ToString().Length;
+
+            int tempNumber = givenNumber; 
+            int sum = 0;  
+
+    
+            while (tempNumber > 0)
+            {
+                int digit = tempNumber % 10;  
+                sum += (int)Math.Pow(digit, digitCount);  
+                tempNumber /= 10;  
+            }
+
+
+            if (sum == givenNumber)
+            {
+                Console.WriteLine($"{givenNumber} is an Armstrong number.");
+            }
+            else
+            {
+                Console.WriteLine($"{givenNumber} is not an Armstrong number.");
+            }
+        }
+    }
+}
