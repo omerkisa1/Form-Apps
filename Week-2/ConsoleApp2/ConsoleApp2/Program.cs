697b5235c0b5bc8ae08fcc89817456c23442ff82
omerkisa1 <omereis.5555@gmail.com>
2024-10-23 14:01:45 +0300

    week 2 question 2 added

---
+++ Week-2/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+       
+        Console.WriteLine("Lütfen bir metin girin:");
+        string input = Console.ReadLine();
+
+      
+        string[] words = input.Split(new[] { ' ', '.', ',', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+        
+        Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+      
+        foreach (var word in words)
+        {
+            string lowerWord = word.ToLower(); 
+            if (wordCount.ContainsKey(lowerWord))
+            {
+                wordCount[lowerWord]++;
+            }
+            else
+            {
+                wordCount[lowerWord] = 1;
+            }
+        }
+
+        Console.WriteLine("\nKelimelerin frekansları:");
+        foreach (var item in wordCount.OrderBy(w => w.Key)) 
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+    }
+}
