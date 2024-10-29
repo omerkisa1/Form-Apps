414b906c5fe4c1c8453e3e6b66fd211642917048
omerkisa1 <omereis.5555@gmail.com>
2024-10-29 12:40:16 +0300

    week 1 question 1 added

---
+++ Week-3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numbers = new List<int>();
+            string input;
+            Console.WriteLine("Enter a number to add an element to the array(press q to exit):");
+
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+                input = Console.ReadLine();
+
+                if(input.ToLower() == "q")
+                {
+                    break;
+                }
+                numbers.Add(int.Parse(input));
+                Console.WriteLine($"Sayı {input} eklendi.");
+            }
+            numbers.Sort();
+            Console.WriteLine("Number you'd like to search: ");
+            string searchNumber = Console.ReadLine();
+
+            int index = Array.BinarySearch(string <numbers>, searchNumber);
+
+        }
+    }
+}
