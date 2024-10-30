10e39c12c8305bd7766471d216b7e5d9f1d2922f
omerkisa1 <omereis.5555@gmail.com>
2024-10-30 14:56:27 +0300

    Q&A 5 in lecture

---
+++ Week-4/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+
+        class Student
+        {
+            string name, surname;
+            private int note;
+
+            public Student(string name, string surname)
+            {
+                this.name = name;
+                this.surname = surname;
+
+            }
+            int getNote()
+            {
+                return this.note;
+            }
+            void setNote(int x )
+            {
+                if (x<0 || x > 100)
+                {
+                    Console.WriteLine("Notes has to be betwwen 0-100");
+                    return;
+                }
+                this.note = x;
+            }
+            void fullName()
+            {
+                Console.WriteLine("Name: " + this.name + " Surname: " + this.surname);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Student student = new Student("Omer", "Kisa");
+
+        }
+    }
+}
