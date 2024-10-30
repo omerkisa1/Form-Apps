1ce3caa8ba4241ce334b451e84e80e137294a064
omerkisa1 <omereis.5555@gmail.com>
2024-10-30 13:56:18 +0300

    question 2 in lecture

---
+++ Week-4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Kitap
+    {
+        string baslik;
+        string yazar;
+        int sayfaSayisi = 100;
+
+        public Kitap(string baslik, string yazar)
+        {
+            baslik = baslik;
+            yazar = yazar;
+        }
+        public void KitapBilgisiGoster()
+        {
+            Console.WriteLine("baslik: "+ baslik+ "\n" + "Yazar: " + yazar +  "\n" + "sayfa sayisi : " + sayfaSayisi);
+        }
+    }
+
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Kitap kitap = new Kitap("harry potter", "J. K. Rowling");
+            kitap.KitapBilgisiGoster();
+        }
+    }
+}
