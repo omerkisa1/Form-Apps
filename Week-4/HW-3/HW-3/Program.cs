30d9590d29bc96b56c4c9404e9d69519fcc2e583
omerkisa1 <omereis.5555@gmail.com>
2024-11-01 11:25:54 +0300

    hw-4 solution

--- Week-4/HW-3/HW-3/Program.cs
+++ Week-4/HW-3/HW-3/Program.cs
@@ -12,17 +12,56 @@ namespace HW_3
         {
             string plate;
             decimal rentPaymentPerDay;
-            bool avaliable;
+            bool available;
+
             public RentACar(string plate, decimal rentPaymentPerDay)
             {
                 this.plate = plate;
                 this.rentPaymentPerDay = rentPaymentPerDay;
-                this.avaliable = true;
+                this.available = true;
+            }
+
+            public bool RentCar()
+            {
+                if (available)
+                {
+                    available = false;
+                    Console.WriteLine($"The car with plate {plate} has been rented.");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine($"The car with plate {plate} is already rented.");
+                    return false;
+                }
+            }
+
+            public bool ReturnCar()
+            {
+                if (!available)
+                {
+                    available = true;
+                    Console.WriteLine($"The car with plate {plate} has been returned.");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine($"The car with plate {plate} was not rented.");
+                    return false;
+                }
             }
         }
 
         static void Main(string[] args)
         {
+            RentACar car1 = new RentACar("34ABC123", 100);
+
+   
+            car1.RentCar(); 
+            car1.RentCar(); 
+            
+            car1.ReturnCar(); 
+            car1.ReturnCar(); 
         }
     }
 }
---
+++ Week-4/HW-4/HW-4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_4
+{
+    class Program
+    {
+        class Phone
+        {
+            string name, surname, phoneNumber;
+            
+            public Phone(string name, string surname, string phoneNumber)
+            {
+                this.name = name;
+                this.surname = surname;
+                this.phoneNumber = phoneNumber;
+            }
+            public void GetContactInfo()
+            {
+                Console.WriteLine($"Name: {name} {surname}, Phone Number: {phoneNumber}");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Phone phone1 = new Phone("Omer", "Kisa", "XXXXXXXXXXXX");
+            phone1.GetContactInfo();
+        }
+    }
+}
