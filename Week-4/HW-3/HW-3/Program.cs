bd8ae90ca7932e432c15420e9644734a2ee2e522
omerkisa1 <omereis.5555@gmail.com>
2024-11-01 11:10:11 +0300

    hw-3 solution

---
+++ Week-4/HW-3/HW-3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_3
+{
+    class Program
+    {
+        class RentACar
+        {
+            string plate;
+            decimal rentPaymentPerDay;
+            bool avaliable;
+            public RentACar(string plate, decimal rentPaymentPerDay)
+            {
+                this.plate = plate;
+                this.rentPaymentPerDay = rentPaymentPerDay;
+                this.avaliable = true;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
