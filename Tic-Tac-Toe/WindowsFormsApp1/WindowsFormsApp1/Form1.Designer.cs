f3bdcb41e3c7625f78c673729895229f338e9d9b
omerkisa1 <omereis.5555@gmail.com>
2024-11-06 11:07:50 +0300

    tic-tac-toe added

---
+++ Tic-Tac-Toe/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn00 = new System.Windows.Forms.Button();
+            this.btn01 = new System.Windows.Forms.Button();
+            this.btn02 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn20 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.btnComputer = new System.Windows.Forms.Button();
+            this.btnFriend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn00
+            // 
+            this.btn00.Location = new System.Drawing.Point(233, 30);
+            this.btn00.Name = "btn00";
+            this.btn00.Size = new System.Drawing.Size(100, 100);
+            this.btn00.TabIndex = 0;
+            this.btn00.UseVisualStyleBackColor = true;
+            // 
+            // btn01
+            // 
+            this.btn01.Location = new System.Drawing.Point(339, 30);
+            this.btn01.Name = "btn01";
+            this.btn01.Size = new System.Drawing.Size(100, 100);
+            this.btn01.TabIndex = 1;
+            this.btn01.UseVisualStyleBackColor = true;
+            // 
+            // btn02
+            // 
+            this.btn02.Location = new System.Drawing.Point(445, 30);
+            this.btn02.Name = "btn02";
+            this.btn02.Size = new System.Drawing.Size(100, 100);
+            this.btn02.TabIndex = 2;
+            this.btn02.UseVisualStyleBackColor = true;
+            // 
+            // btn10
+            // 
+            this.btn10.Location = new System.Drawing.Point(233, 136);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(100, 100);
+            this.btn10.TabIndex = 3;
+            this.btn10.UseVisualStyleBackColor = true;
+            // 
+            // btn11
+            // 
+            this.btn11.Location = new System.Drawing.Point(339, 136);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(100, 100);
+            this.btn11.TabIndex = 4;
+            this.btn11.UseVisualStyleBackColor = true;
+            // 
+            // btn12
+            // 
+            this.btn12.Location = new System.Drawing.Point(445, 136);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(100, 100);
+            this.btn12.TabIndex = 5;
+            this.btn12.UseVisualStyleBackColor = true;
+            // 
+            // btn20
+            // 
+            this.btn20.Location = new System.Drawing.Point(233, 242);
+            this.btn20.Name = "btn20";
+            this.btn20.Size = new System.Drawing.Size(100, 100);
+            this.btn20.TabIndex = 6;
+            this.btn20.UseVisualStyleBackColor = true;
+            // 
+            // btn21
+            // 
+            this.btn21.Location = new System.Drawing.Point(339, 242);
+            this.btn21.Name = "btn21";
+            this.btn21.Size = new System.Drawing.Size(100, 100);
+            this.btn21.TabIndex = 7;
+            this.btn21.UseVisualStyleBackColor = true;
+            // 
+            // btn22
+            // 
+            this.btn22.Location = new System.Drawing.Point(445, 242);
+            this.btn22.Name = "btn22";
+            this.btn22.Size = new System.Drawing.Size(100, 100);
+            this.btn22.TabIndex = 8;
+            this.btn22.UseVisualStyleBackColor = true;
+            this.btn22.Click += new System.EventHandler(this.btn22_Click);
+            // 
+            // btnComputer
+            // 
+            this.btnComputer.Location = new System.Drawing.Point(282, 395);
+            this.btnComputer.Name = "btnComputer";
+            this.btnComputer.Size = new System.Drawing.Size(90, 23);
+            this.btnComputer.TabIndex = 9;
+            this.btnComputer.Text = "Computer";
+            this.btnComputer.UseVisualStyleBackColor = true;
+            this.btnComputer.Click += new System.EventHandler(this.btnComputer_Click);
+            // 
+            // btnFriend
+            // 
+            this.btnFriend.Location = new System.Drawing.Point(391, 395);
+            this.btnFriend.Name = "btnFriend";
+            this.btnFriend.Size = new System.Drawing.Size(132, 23);
+            this.btnFriend.TabIndex = 10;
+            this.btnFriend.Text = "Play with friends";
+            this.btnFriend.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFriend);
+            this.Controls.Add(this.btnComputer);
+            this.Controls.Add(this.btn22);
+            this.Controls.Add(this.btn21);
+            this.Controls.Add(this.btn20);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn02);
+            this.Controls.Add(this.btn01);
+            this.Controls.Add(this.btn00);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn00;
+        private System.Windows.Forms.Button btn01;
+        private System.Windows.Forms.Button btn02;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn20;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Button btnComputer;
+        private System.Windows.Forms.Button btnFriend;
+    }
+}
+
---
+++ Tic-Tac-Toe/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private bool isXTurn = true; // Track if it's X's turn or O's
+        private bool isVsComputer = false; // Game mode: true if vs computer
+        private Random random = new Random(); // For computer's random move
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Attach event handler to each button on the grid
+            btn00.Click += new EventHandler(button_Click);
+            btn01.Click += new EventHandler(button_Click);
+            btn02.Click += new EventHandler(button_Click);
+            btn10.Click += new EventHandler(button_Click);
+            btn11.Click += new EventHandler(button_Click);
+            btn12.Click += new EventHandler(button_Click);
+            btn20.Click += new EventHandler(button_Click);
+            btn21.Click += new EventHandler(button_Click);
+            btn22.Click += new EventHandler(button_Click);
+
+            // Set game mode (vs Computer or vs Friend)
+            btnComputer.Click += delegate { isVsComputer = true; ResetBoard(); };
+            btnFriend.Click += delegate { isVsComputer = false; ResetBoard(); };
+        }
+
+        // Handles the player's move
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            // Check if the button is empty before marking it
+            if (button.Text == "")
+            {
+                // Mark with X or O depending on the turn
+                button.Text = isXTurn ? "X" : "O";
+                isXTurn = !isXTurn; // Switch turn
+                CheckForWinner(); // Check if anyone won
+
+                // If playing vs computer and it's computer's turn, make computer move
+                if (isVsComputer && !isXTurn)
+                {
+                    ComputerMove();
+                    CheckForWinner();
+                }
+            }
+            else
+            {
+                MessageBox.Show("This button is already taken, choose another.");
+            }
+        }
+
+        // Computer makes a random move
+        private void ComputerMove()
+        {
+            Button[] buttons = { btn00, btn01, btn02, btn10, btn11, btn12, btn20, btn21, btn22 };
+            Button emptyButton;
+
+            // Keep choosing a random button until we find an empty one
+            do
+            {
+                emptyButton = buttons[random.Next(buttons.Length)];
+            } while (emptyButton.Text != "");
+
+            emptyButton.Text = "O"; // Computer marks O
+            isXTurn = !isXTurn; // Switch back to player's turn
+        }
+
+        // Check if there's a winner or a tie
+        private void CheckForWinner()
+        {
+            string[,] board = new string[3, 3];
+
+            // Copy button texts into a 2D array for easier checks
+            board[0, 0] = btn00.Text; board[0, 1] = btn01.Text; board[0, 2] = btn02.Text;
+            board[1, 0] = btn10.Text; board[1, 1] = btn11.Text; board[1, 2] = btn12.Text;
+            board[2, 0] = btn20.Text; board[2, 1] = btn21.Text; board[2, 2] = btn22.Text;
+
+            // Check if X or O has a winning line
+            if (CheckLines(board, "X"))
+            {
+                MessageBox.Show("X wins!");
+                ResetBoard();
+            }
+            else if (CheckLines(board, "O"))
+            {
+                MessageBox.Show("O wins!");
+                ResetBoard();
+            }
+            else if (IsBoardFull())
+            {
+                MessageBox.Show("It's a tie!");
+                ResetBoard();
+            }
+        }
+
+        // Checks for 3 in a row for the given player
+        private bool CheckLines(string[,] board, string player)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                // Check rows and columns
+                if ((board[i, 0] == player && board[i, 1] == player && board[i, 2] == player) ||
+                    (board[0, i] == player && board[1, i] == player && board[2, i] == player))
+                    return true;
+            }
+
+            // Check diagonals
+            if ((board[0, 0] == player && board[1, 1] == player && board[2, 2] == player) ||
+                (board[0, 2] == player && board[1, 1] == player && board[2, 0] == player))
+                return true;
+
+            return false;
+        }
+
+        // Checks if all buttons are filled (for a tie)
+        private bool IsBoardFull()
+        {
+            foreach (Control control in Controls)
+            {
+                Button button = control as Button;
+                if (button != null && button.Text == "")
+                    return false;
+            }
+            return true;
+        }
+
+        // Reset the board for a new game
+        private void ResetBoard()
+        {
+            foreach (Control control in Controls)
+            {
+                Button button = control as Button;
+                if (button != null)
+                    button.Text = "";
+            }
+            isXTurn = true; // X starts again
+        }
+        private void btn22_Click(object sender, EventArgs e)
+        {
+            // just pass this
+        }
+
+        private void btnComputer_Click(object sender, EventArgs e)
+        {
+            // just pass this
+        }
+    }
+}
