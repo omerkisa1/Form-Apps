5308cc9259f6f2ce87af5e190253d477681321bf
omerkisa1 <omereis.5555@gmail.com>
2024-11-19 00:59:13 +0300

    hw aggregation 1 added

---
+++ Week-6/Aggregation1/Aggregation1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aggregation1
+{
+    class Program
+    {
+        class Home
+        {
+            public string Name { get; set; }
+            public List<Room> Rooms { get; set; }
+
+            public Home(string name)
+            {
+                Name = name;
+                Rooms = new List<Room>();
+            }
+            public void addRoom(Room room)
+            {
+                Rooms.Add(room);
+            }
+        }
+
+        class Room
+        {
+            public string area { get; set; }
+            public string type { get; set; }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
