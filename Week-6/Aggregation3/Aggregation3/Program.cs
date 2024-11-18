978b1a7b1b8581765eb6b898db9882f36c88f373
omerkisa1 <omereis.5555@gmail.com>
2024-11-19 01:10:43 +0300

    hw aggregation 3 added

---
+++ Week-6/Aggregation3/Aggregation3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aggregation2
+{
+    class Library
+    {
+        public string Name { get; set; }
+        public List<Book> Books { get; set; }
+
+        public Library(string name)
+        {
+            Name = name;
+            Books = new List<Book>();
+        }
+
+        public void AddBook(Book book)
+        {
+            Books.Add(book);
+        }
+    }
+
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library("Central Library");
+            Book book1 = new Book("1984", "George Orwell");
+            Book book2 = new Book("To Kill a Mockingbird", "Harper Lee");
+
+            library.AddBook(book1);
+            library.AddBook(book2);
+
+            foreach (var book in library.Books)
+            {
+                Console.WriteLine($"Title: {book.Title}, Author: {book.Author}");
+            }
+        }
+    }
+}
