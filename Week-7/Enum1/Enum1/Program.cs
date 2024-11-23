a272a4be1a0d15caaf0312c2cb1e2fa0b23f2b15
omerkisa1 <omereis.5555@gmail.com>
2024-11-23 22:14:11 +0300

    enum1 added

---
+++ Week-7/Enum1/Enum1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+enum TrafficLight
+{
+    Red,
+    Yellow,
+    Green
+}
+
+class TrafficLightController
+{
+    public string GetAction(TrafficLight light)
+    {
+        switch (light)
+        {
+            case TrafficLight.Red:
+                return "Stop!";
+            case TrafficLight.Yellow:
+                return "Get Ready!";
+            case TrafficLight.Green:
+                return "Go!";
+            default:
+                return "Unknown State";
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        TrafficLightController trafficController = new TrafficLightController();
+        Console.WriteLine("Kırmızı Işık Durumu: " + trafficController.GetAction(TrafficLight.Red)); 
+        Console.WriteLine("Sarı Işık Durumu: " + trafficController.GetAction(TrafficLight.Yellow)); 
+        Console.WriteLine("Yeşil Işık Durumu: " + trafficController.GetAction(TrafficLight.Green)); 
+
+        Console.ReadLine();
+    }
+}
