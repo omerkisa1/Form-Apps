1f3550372410c2f2012cfa8be3c0c379ee31bfdc
omerkisa1 <omereis.5555@gmail.com>
2024-11-23 22:19:22 +0300

    enum 2 added

---
+++ Week-7/Enum2/Enum2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+enum Weather
+{
+    Sunny,
+    Rainy,
+    Cloudy,
+    Stormy
+
+}
+
+class WeatherController
+{
+    public string GetAction(Weather stat)
+    {
+        switch (stat)
+        {
+            case Weather.Sunny:
+                return "Time to go outside";
+            case Weather.Rainy:
+                return "Do not forget your umbrella";
+            case Weather.Cloudy:
+                return "It's fine take care to about rain anyway";
+            case Weather.Stormy:
+                return "Don't go outside!";
+            default:
+                return "Unknown State";
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        WeatherController weather = new WeatherController();
+        Console.WriteLine("Weather: " + weather.GetAction(Weather.Sunny));
+        Console.WriteLine("Weather: " + weather.GetAction(Weather.Rainy));
+        Console.WriteLine("Weather: " + weather.GetAction(Weather.Cloudy));
+        Console.WriteLine("Weather: " + weather.GetAction(Weather.Stormy));
+
+        Console.ReadLine();
+    }
+}
