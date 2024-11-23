4309abfddd311cc675dabbafd13e6910242514c6
omerkisa1 <omereis.5555@gmail.com>
2024-11-23 17:52:10 +0300

    overloading 2 added

---
+++ Week-7/OverLoadingHW2/OverLoadingHW2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoadingHW2
+{
+    class Program
+    {
+        const double PI = 3.14;
+        public int Area(int a)
+        {
+            return a*a;
+        }
+        public int Area(int a, int b)
+        {
+            return a*b;
+        }
+        public double Area(double r)
+        {
+            return PI*r*r;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
