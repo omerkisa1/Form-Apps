5e8e4ab5b12c299ca899c56029a6138e9b760ecd
omerkisa1 <omereis.5555@gmail.com>
2024-11-23 18:08:25 +0300

    indexer hw2 added

---
+++ Week-7/IndexerHW2/IndexerHW2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+class StudentGrades
+{
+
+    private Dictionary<string, int> grades = new Dictionary<string, int>();
+
+
+    public int this[string courseName]
+    {
+        get
+        {
+            if (grades.ContainsKey(courseName))
+            {
+                return grades[courseName]; 
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Ders '{courseName}' bulunamadı.");
+            }
+        }
+        set
+        {
+            grades[courseName] = value; 
+    }
+
+  
+    public void PrintAllGrades()
+    {
+        foreach (var course in grades)
+        {
+            Console.WriteLine($"Ders: {course.Key}, Not: {course.Value}");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StudentGrades studentGrades = new StudentGrades();
+
+       
+        studentGrades["Matematik"] = 90;
+        studentGrades["Fizik"] = 85;
+        studentGrades["Kimya"] = 78;
+
+  
+        Console.WriteLine("Matematik Notu: " + studentGrades["Matematik"]); 
+        Console.WriteLine("Fizik Notu: " + studentGrades["Fizik"]);       
+
+    
+        try
+        {
+            Console.WriteLine(studentGrades["Biyoloji"]); 
+        }
+        catch (KeyNotFoundException ex)
+        {
+            Console.WriteLine(ex.Message); 
+        }
+
+    
+        studentGrades.PrintAllGrades();
+
+        Console.ReadLine();
+    }
+}
