6698bc463c9fe7d302bd893c5991cbe554c3a73d
omerkisa1 <omereis.5555@gmail.com>
2025-01-13 21:02:09 +0300

    add files

---
+++ FileExample/FileTest1/FileTest1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileTest1
+{
+    class Program
+    {
+
+        public class HataYonetimiOrnekleri
+        {
+            public void DosyaIslemleri()
+            {
+                try
+                {
+                    // Riskli kod bloğu
+                    string dosyaIcerigi = File.ReadAllText("C:\\Users\\omere\\Desktop\\test.txt");
+                    Console.WriteLine(dosyaIcerigi);
+                }
+                catch (FileNotFoundException ex)
+                {
+                    // Spesifik hata yakalama
+                    Console.WriteLine($"Dosya bulunamadı: {ex.Message}");
+                    // Çıktı: Dosya bulunamadı: Could not find file 'olmayan_dosya.txt'
+                }
+                catch (Exception ex)
+                {
+                    // Genel hata yakalama
+                    Console.WriteLine($"Bir hata oluştu: {ex.Message}");
+                }
+                finally
+                {
+                    // Her durumda çalışacak kod
+                    Console.WriteLine("İşlem tamamlandı");
+                    // Çıktı: İşlem tamamlandı
+                }
+            }
+
+
+            static void Main(string[] args)
+            {
+                HataYonetimiOrnekleri a = new HataYonetimiOrnekleri();
+                a.DosyaIslemleri();
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Pointers/App1/App1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int* ptr = &x; 
+
+            Console.WriteLine($"Değer: {x}");
+            Console.WriteLine($"Adres: {(int)ptr}");
+
+            *ptr = 20; 
+            Console.WriteLine($"Yeni Değer: {x}");
+        }
+    }
+}
