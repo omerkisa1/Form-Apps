9b545ca81b53e192f36bda454250e389cfcc8371
omerkisa1 <omereis.5555@gmail.com>
2025-01-13 22:23:30 +0300

    timer example

---
+++ FormTest/Timer/Timer/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int counter = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            counter++;
+            label1.Text += counter;
+
+        }
+    }
+}
