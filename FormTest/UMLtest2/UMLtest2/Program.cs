012cdebd9c099d8722ed872bbd92f1303633e51a
omerkisa1 <omereis.5555@gmail.com>
2025-01-14 19:54:04 +0300

    simple uml test added

---
+++ FormTest/UMLtest2/UMLtest2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMLtest2
+{
+    class Program
+    {
+        class Ogretmen
+        {
+            public string İsim { get; set; }
+            public List<Ders> dersler = new List<Ders>();
+            public Ogretmen(string isim)
+            {
+                this.İsim = isim; 
+            }
+            public void DersEkle(Ders ders)
+            {
+                if(dersler.Contains(ders))
+                {
+                    Console.WriteLine("Bu ders zaten mevcut");
+                }
+                else
+                {
+                    dersler.Add(ders);
+                    Console.WriteLine($"{ders.Adı} dersi eklendi");
+                }
+            }
+            public void DersleriListele()
+            {
+                foreach(var ders in dersler)
+                {
+                    Console.WriteLine(ders.Adı +"\n");
+                }
+            }
+
+
+        }
+        class Ders
+        {
+            public string Adı { get; set; }
+
+            public Ders(string adi)
+            {
+                this.Adı = adi;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Ogretmen ogr = new Ogretmen("Omer");
+            Ders drs1 = new Ders("matematik");
+            Ders drs2 = new Ders("fizik");
+            Ders drs3 = new Ders("kimya");
+
+            ogr.DersEkle(drs1);
+            ogr.DersEkle(drs2);
+            ogr.DersEkle(drs3);
+            ogr.DersleriListele();
+            Console.ReadKey();
+
+        }
+    }
+}
